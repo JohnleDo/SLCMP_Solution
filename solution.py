04b0b04d2f2919074c54cfa64b0dc958f2763e3d
unknown <kitsuneace@gmail.com>
2020-05-25 10:19:55 -0500

    Updated comments

--- solution.py
+++ solution.py
@@ -24,10 +24,9 @@ class SLCMP:
         When we get that list we will then check if we have more than one rate_area for that zipcode so that we can avoid looking for a
         second low cost for it due to it being ambigous with two rate_areas instead of one. Now that we have a list we then iterate through
         all the potential zipcodes we got from self.zips and try to find a match in our self.plans dataframe with the extra criteria of "rate_area".
-        If we get a match then we store it into a list called info (reason for storing it into a list is because there can be the case of multiple
-        matches of even no matches). Then we check if our info list is empty, if not then we take the first value it has in the list because from
-        the previous functions we already got rid of the lowest value and they should be organized from smallest to biggest value. And with all
-        that we add that value to the column "rate" for that zipcode in self.slcmp
+        If we get a match then we store it into a list called info. Then we check if our info list is empty, if not then we take the first value it
+        has in the list since there should only be one match unless we have a duplicate then it still doesn't matter and set it to our SLCSP in the rate
+        column.
         """
         for slcmp_index, slcmp_row in self.slcmp.iterrows():
             zipcodes = self.zips.loc[(self.zips["zipcode"] == slcmp_row["zipcode"])]
